dc07714bfd5bbe376b815d25f64f411ac51ec294
jacob-tucker <jacobtucker818@gmail.com>
2021-11-30 23:01:05 -0600

    add first 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import saminaAudio from "./audio/samina.m4a";
+import gelAudio from "./audio/gel.m4a";
+import classNames from 'classnames';
 
-function App() {
+const gelSummary = `Gel and I work together at Decentology and have known each other for about 4 months now. We are both a part of 
+the Blockchain industry and have had tons of fun developing together. One of the cool things about being a developer in a new industry
+is that you get to decide what goes and what doesn't. Gel is a big part of helping to include more women in the Blockchain space, and
+in this episode, we go over how she does it and what we can all do to help more women feel included in the Web3 space.`
+
+const saminaSummary = `Samina and I have worked together for about 4 months at Decentology. Samina is a Developer Advocate and has a
+very unique perspective on becoming a Computer Scientist as a woman. She describes her own past, how she transitioned out of the
+medical field into Computer Science, and how we can encourage other women to understand that Computer Science is a possible major
+to pursue.`
+
+const App = () => {
+  const [summary, setSummary] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1>Jacob Tucker's Episodes</h1>
+      </nav>
+
+      <div className={classNames({
+        nodisplay: summary === "",
+        description: true
+      })}>
+        <h1>Description</h1>
+        <p>{summary}</p>
+        <button onClick={() => setSummary("")}>Close</button>
+      </div>
+
+      <div className="list-of-podcasts">
+        <div className="podcast podcast1">
+          <h2>Women in Tech</h2>
+          <audio controls src={saminaAudio}>Play</audio>
+          <h3>ft. Samina Kabir</h3>
+          <button onClick={() => setSummary(saminaSummary)}>View Summary</button>
+        </div>
+
+        <div className="podcast podcast2">
+          <h2>Women in Tech</h2>
+          <audio controls src={gelAudio}>Play</audio>
+          <h3>ft. Angelica Turla</h3>
+          <button onClick={() => setSummary(gelSummary)}>View Summary</button>
+        </div>
+      </div>
     </div>
   );
 }
